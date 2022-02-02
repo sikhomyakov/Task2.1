ade39c8f1909ddc188826347e384e2780368bc21
Stanislav Khomyakov <sikhomyakov@gmail..com>
2022-02-03 00:31:49 +0500

    Task 2.1 Final

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,8 +3,8 @@ package ru.netology
 fun main() {
     val amount = 12165_00
     val minDiscount = 35_00
-    val discount = amount * 0.0075
 
+    val discount = amount * 0.0075
     val result = if (discount <= minDiscount) minDiscount else discount
     println("Ваша комиссия составляет: ${result.toInt() / 100} рублей ${result.toInt() % 100} копеек")
 }
